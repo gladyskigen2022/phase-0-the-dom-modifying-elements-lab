aae130d28c8065cb84cb0e5063af979a20673bcb
GladysKigen <gladys.kigen@student.moringa.com>
2022-11-29 21:30:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+//document.querySelector('main');
+const element = document.getElementById('main');
+document.getElementById('main');
+element.remove('main');
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML = "YOUR-NAME is the champion";
+
+//'newHeader' variable that points to node 
